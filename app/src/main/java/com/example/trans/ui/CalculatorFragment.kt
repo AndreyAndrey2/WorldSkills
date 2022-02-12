c843c5edc4d3f22080c4d7ba172de80e5179ec1e
Andrey Semin <a.semin@profsoft.pro>
2022-02-12 12:33:12 +0400

    add sum fun

--- app/src/main/java/com/example/trans/ui/CalculatorFragment.kt
+++ app/src/main/java/com/example/trans/ui/CalculatorFragment.kt
@@ -1,16 +1,25 @@
 package com.example.trans.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.trans.R
+import com.example.trans.databinding.ActivityMainBinding
+import com.example.trans.databinding.FragmentCalculatorBinding
 
 class CalculatorFragment : Fragment() {
 
+    private lateinit var binding: FragmentCalculatorBinding
+    var result: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -18,6 +27,19 @@ class CalculatorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculator, container, false)
+        binding = FragmentCalculatorBinding.inflate(layoutInflater)
+
+        binding.btnSum.setOnClickListener{
+            val a: Int = binding.text1.text.toString().toInt()
+            val b: Int = binding.text2.text.toString().toInt()
+
+
+            binding.kirill.text = "Результат: ${a + b}"
+        }
+
+
+        return binding.root
+
     }
 }
+
